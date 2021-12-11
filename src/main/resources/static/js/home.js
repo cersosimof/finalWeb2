5b03310f274f7952dd6047ee47364a7fbd6aee9a
cersosimof <cersosimo.facundo@gmail.com>
2021-12-11 12:03:56 -0300

    avacnce

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,5 +1,5 @@
 function verProducto(i) {
     let nroProducto = i.split("_")[1];
 
-    location.href = "/verProducto/" + nroProducto
+    location.href = "/instrumentos/verProducto/" + nroProducto
 }
